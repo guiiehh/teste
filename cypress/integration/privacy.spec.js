2a26bd40004e15f1dd39f774fab0b5e3080e2c47
klemosb <karinnylemosb@gmail.com>
2024-04-06 13:24:49 -0200

    Adicionando ci.pipeline

--- cypress/integration/privacy.spec.js
+++
@@ -1,4 +0,0 @@
-it.only ('testa a página da política de privacidade de forma independente', function(){
-    cy.visit('./src/privacy.html')
-    cy.contains('Talking About Testing').should('be.visible')
-})
