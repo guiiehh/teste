c8adc356936094bcf2af66a9b1b75464532c863d
¡Duque <atanaelleonardo@gmail.com>
2024-04-18 19:04:17 -0300

    "Updated cypress.config.js to remove baseUrl and added visit command in CAC-TAT.cy.js"
    The diff includes changes to the cypress.config.js file where the baseUrl property is commented out

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl:'./src/index.html',
+    // baseUrl:'',
     // pluginsFile: false,
     viewportHeight: 880,
     viewportWidth: 1280,
--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -1,5 +1,6 @@
 describe('Central de Atendimento ao Cliente TAT', function() {
   it('verifica o título da aplicação', function() {
-
+    cy.visit('./src/index.html')
+      .title().should('be.equal', 'Central de Atendimento ao Cliente TAT');
   })
 })
