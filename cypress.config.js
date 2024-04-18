b88de948aace065c4fc460f337a09b23453895c9
¡Duque <atanaelleonardo@gmail.com>
2024-04-18 19:00:52 -0300

    Change cypress configuration: set baseUrl to './src/index.html' and update spec file to test CAC-TAT application.
    The cypress.config.js file has been updated to set the baseUrl to './src

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,8 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    pluginsFile: false,
+    baseUrl:'./src/index.html',
+    // pluginsFile: false,
     viewportHeight: 880,
     viewportWidth: 1280,
   },
--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -1,5 +1,5 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Central de Atendimento ao Cliente TAT', function() {
+  it('verifica o título da aplicação', function() {
+
   })
 })
