90dfa4a8b4d6b1d563240e80bf1725da7199687d
¡Duque <atanaelleonardo@gmail.com>
2024-04-18 20:40:38 -0300

    "Updated form submission test to use chained commands for assertions and input"
    This commit updates the test for submitting a form in the Central de Atendimento ao Cliente TAT by using chained commands for asserting visibility,

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -15,11 +15,23 @@ describe('Central de Atendimento ao Cliente TAT', function () {
   });
 
   it('preenche os campos obrigatórios e envia o formulário', function () {
-    cy.get('input[id="firstName"]').should('be.visible').type(form.name).should('have.value', form.name)
-    cy.get('input[id="lastName"]').should('be.visible').type(form.lastName).should('have.value', form.lastName)
-    cy.get('input[id="email"]').should('be.visible').type(form.email).should('have.value', form.email)
-    cy.get('textarea[id="open-text-area"]').should('be.visible').type(form.howWeMayHelp).should('have.value', form.howWeMayHelp)
-    cy.get('button[type="submit"]').should('be.visible').click()
-    cy.get('span[class="success"]').should('be.visible')
+    cy.get('input[id="firstName"]')
+      .should('be.visible')
+      .type(form.name)
+      .should('have.value', form.name);
+    cy.get('input[id="lastName"]')
+      .should('be.visible')
+      .type(form.lastName)
+      .should('have.value', form.lastName);
+    cy.get('input[id="email"]')
+      .should('be.visible')
+      .type(form.email)
+      .should('have.value', form.email);
+    cy.get('textarea[id="open-text-area"]')
+      .should('be.visible')
+      .type(form.howWeMayHelp)
+      .should('have.value', form.howWeMayHelp);
+    cy.get('button[type="submit"]').should('be.visible').click();
+    cy.get('span[class="success"]').should('be.visible');
   });
 });
