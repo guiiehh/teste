8967f518bee034e6ea979f7fa32aff000f0d488b
¡Duque <atanaelleonardo@gmail.com>
2024-04-18 20:39:37 -0300

    "Adds email input validation in CAC-TAT test"
    This commit adds a new step to the existing test that checks the 'email' input field for visibility, types in the email value from the 'form' object, and

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -17,6 +17,7 @@ describe('Central de Atendimento ao Cliente TAT', function () {
   it('preenche os campos obrigatórios e envia o formulário', function () {
     cy.get('input[id="firstName"]').should('be.visible').type(form.name).should('have.value', form.name)
     cy.get('input[id="lastName"]').should('be.visible').type(form.lastName).should('have.value', form.lastName)
+    cy.get('input[id="email"]').should('be.visible').type(form.email).should('have.value', form.email)
 
   });
 });
