4bcb9daf4c9b7afd0cc2ed3f10afa348b1e5f7ff
¡Duque <atanaelleonardo@gmail.com>
2024-04-18 20:39:25 -0300

    "Added cypress test step to fill last name field"
    This commit adds a new test step to the Cypress end-to-end test for the Central de Atendimento ao Cliente TAT. The new step fills in

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -16,6 +16,7 @@ describe('Central de Atendimento ao Cliente TAT', function () {
 
   it('preenche os campos obrigatórios e envia o formulário', function () {
     cy.get('input[id="firstName"]').should('be.visible').type(form.name).should('have.value', form.name)
+    cy.get('input[id="lastName"]').should('be.visible').type(form.lastName).should('have.value', form.lastName)
 
   });
 });
