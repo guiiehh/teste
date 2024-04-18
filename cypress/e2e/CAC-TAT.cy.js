a88b828a36556600f1fd4b50b04f5770b0eeee3f
¡Duque <atanaelleonardo@gmail.com>
2024-04-18 20:39:12 -0300

    "Added form data and beforeEach hook in CAC-TAT.cy.js"
    This commit includes the following changes:
    - Added a `form` object with form data at the beginning of the file.
    - Add

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -1,7 +1,21 @@
+const form = {
+  name: 'John',
+  lastName: 'Doe',
+  email: 'john.doe@email.com',
+  howWeMayHelp: 'Testing message...',
+};
+
 describe('Central de Atendimento ao Cliente TAT', function () {
+  beforeEach(() => {
+    cy.visit('./src/index.html');
+  });
+
   it('verifica o título da aplicação', function () {
-    cy.visit('./src/index.html')
-      .title()
-      .should('be.equal', 'Central de Atendimento ao Cliente TAT');
+    cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT');
+  });
+
+  it('preenche os campos obrigatórios e envia o formulário', function () {
+    cy.get('input[id="firstName"]').should('be.visible').type(form.name).should('have.value', form.name)
+
   });
 });
