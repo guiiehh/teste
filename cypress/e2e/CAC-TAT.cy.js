0c36bba6ca2a7892b4a90db296ed293037c1f272
¡Duque <atanaelleonardo@gmail.com>
2024-04-19 12:07:05 -0300

    "Added new test case for accessing privacy policy page by removing target attribute from the link"

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -192,4 +192,14 @@ describe('Central de Atendimento ao Cliente TAT', () => {
       .should('be.visible')
       .should('have.attr', 'target', '_blank')
   })
+  it.only('acessa a página da política de privacidade removendo o target e então clicando no link', () => {
+    cy.get('#privacy a')
+      .should('be.visible')
+      .should('have.attr', 'target', '_blank')
+      .invoke('removeAttr', 'target')
+      .click()
+    cy.get('#white-background > :nth-child(5)').contains(
+      'Talking About Testing',
+    )
+  })
 })
