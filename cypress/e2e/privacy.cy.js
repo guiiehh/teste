385105f4667a0cd45c11b9e27c2a592177ba6925
¡Duque <atanaelleonardo@gmail.com>
2024-04-19 12:12:06 -0300

    Update privacy.cy.js to visit privacy.html directly and update selector for link to privacy policy
    - Before: visited privacy page by clicking on link with target=_blank
    - After: visit privacy.html directly and use a new selector

--- cypress/e2e/privacy.cy.js
+++ cypress/e2e/privacy.cy.js
@@ -1,12 +1,10 @@
 describe('should privacy page be visible', () => {
+  beforeEach(() => {
+    cy.visit('./src/privacy.html')
+  })
   it.only('acessa a página da política de privacidade removendo o target e então clicando no link', () => {
-    cy.get('#privacy a')
+    cy.get('#white-background > :nth-child(5)')
       .should('be.visible')
-      .should('have.attr', 'target', '_blank')
-      .invoke('removeAttr', 'target')
-      .click()
-    cy.get('#white-background > :nth-child(5)').contains(
-      'Talking About Testing',
-    )
+      .contains('Talking About Testing')
   })
 })
