dd04072802cd9391891ffd5382f408436f8d137b
Gabriel Siqueira <gabriel.siqueira@globant.com>
2024-05-24 17:44:52 -0300

    exercicio 5

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,6 +1,7 @@
 /// <reference types="Cypress" />
 
 describe('Central de Atendimento ao Cliente TAT', ()=> {
+    const longText = 'teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, ';
     beforeEach (function(){
         cy.visit('src/index.html');
     })
@@ -10,7 +11,7 @@ describe('Central de Atendimento ao Cliente TAT', ()=> {
     })
 
     it('preenche os campos obrigatórios e envia o formulário', () => {
-        const longText = 'teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, ';
+        
         cy.get('input[id="firstName"]').type('Gabriel Vitor')
         cy.get('input[id="lastName"]').type('Siqueira')
         cy.get('input[id="email"]').type('gabrielvitorsiqueira53@gmail.com')
@@ -38,7 +39,7 @@ describe('Central de Atendimento ao Cliente TAT', ()=> {
         .should('have.value', '')
     })
 
-    it.only('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', function(){
+    it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', function(){
         cy.get('input[id="firstName"]').type('Gabriel Vitor')
         cy.get('input[id="lastName"]').type('Siqueira')
         cy.get('input[id="email"]').type('gabrielvitorsiqueira53@gmail,com')
@@ -46,4 +47,18 @@ describe('Central de Atendimento ao Cliente TAT', ()=> {
 
         cy.get('.error').should('be.visible')
     })
+
+    it.only('preenche e limpa os campos nome, sobrenome, email e telefone', function() {
+        cy.get('#firstName').type('Gabriel Vitor').should('have.value', 'Gabriel Vitor')
+        cy.get('#firstName').clear().should('have.value', '')
+        cy.get('#lastName').type('Siqueira').should('have.value', 'Siqueira')
+        cy.get('#lastName').clear().should('have.value', '')
+        cy.get('#email').type('gabrielvitorsiqueira53@gmail,com').should('have.value', 'gabrielvitorsiqueira53@gmail,com')
+        cy.get('#email').clear().should('have.value', '')
+        cy.get('#phone').type('998645631').should('have.value', '998645631')
+        cy.get('#phone').clear().should('have.value', '')
+        cy.get('#open-text-area'). type(longText). should('have.value', longText)
+        cy.get('#open-text-area').clear().should('have.value', '')
+    })
+
   })
