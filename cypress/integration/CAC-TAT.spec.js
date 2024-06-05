e1bc319af13e5bdcbc1f4912aac926284b07f069
Mauro Moreno <mmauromoreno1@gmail.com>
2024-06-05 20:43:56 -0300

    Aula 2 exercicio 2, 3, 4, 5

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -11,42 +11,44 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     })
 
     it.only('preenche os campos obrigatórios e envia o formulário', function() {
-        cy.get('input[type="text"][name="firstName"]')
-            .should('be.visible')
-            .type('Mauro Moreno')
-            .should('have.value', 'Mauro Moreno')
+        cy.get('#firstName').type('Mauro Moreno')
+        cy.get('#lastName').type('Kühl Neto')
+        cy.get('#email').type('teste@tarolho.com')
+        cy.get('#phone').type('998502232')
+        cy.get('select').select('Cursos')
+        cy.get('textarea').type('Quero comprar uma saveiro rebaixada').wait(0)
+        cy.get('.button[type="submit"]').click()
+        cy.get('.success').should('be.visible')
+    })
 
-        cy.get('input[type="text"][name="lastName"]')
-            .should('be.visible')
-            .type('Kühl Neto')
-            .should('have.value', 'Kühl Neto')
+    it.only('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function() {
+        cy.get('#firstName').type('Mauro Moreno')
+        cy.get('#lastName').type('Kühl Neto')
+        cy.get('#email').type('testeroni')
+        cy.get('#phone').type('998502232')
+        cy.get('select').select('Cursos')
+        cy.get('textarea').type('Quero comprar uma saveiro rebaixada').wait(0)
+        cy.get('.button[type="submit"]').click()
+        cy.get('.error').should('be.visible')
+    })
 
-        cy.get('input[type="email"][name="email"]')
-            .should('be.visible')
-            .type('teste@tarolho.com')
-            .should('have.value', 'teste@tarolho.com')
+    it.only('campo de telefone fica vazio caso seja digitado um valor não numérico', function() {
+        cy.get('#phone').type('tetaroni').should('have.text', '')
+    })
 
-        cy.get('input[type="number"][name="phone"]')
-            .should('be.visible')
-            .type('998502232')
-            .should('have.value', '998502232')
+    /* it.only('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', function () {
+        
+    }) */
 
-        cy.get('select')
-            .select('Cursos')
-            .should('have.value', 'cursos')
-
-        cy.get('textarea')
-            .should('be.visible')
-            .type('Quero comprar uma saveiro rebaixada')
-            .wait(0)
-            .should('have.value', 'Quero comprar uma saveiro rebaixada')
-
-        cy.get('button[type="submit"]')
-            .should('be.visible')
-            .click()
-
-        cy.get('span[class="success"]')
-            .should('be.visible')
+    it.only('preenche e limpa os campos nome, sobrenome, email e telefone', function () {
+        cy.get('#firstName').type('Mauro Moreno').should('have.value', 'Mauro Moreno')
+            cy.get('#firstName').clear().should('have.value', '')
+        cy.get('#lastName').type('Kühl Neto').should('have.value', 'Kühl Neto')
+            cy.get('#lastName').clear().should('have.value', '')
+        cy.get('#email').type('teste@tarolho.com').should('have.value', 'teste@tarolho.com')
+            cy.get('#email').clear().should('have.value', '')
+        cy.get('#phone').type('998502232').should('have.value', '998502232')
+            cy.get('#phone').clear().should('have.value', '')
     })
   })
   
